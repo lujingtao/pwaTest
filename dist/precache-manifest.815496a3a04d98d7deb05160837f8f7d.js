eb8148ff8b73e24bd29661f358b550315313a8f7
lujingtao <lujingtao@countrygarden.com.cn>
2020-01-17 15:08:34 +0800

    add

---
+++ dist/precache-manifest.815496a3a04d98d7deb05160837f8f7d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc7fc7eb990cb17c6f28",
+    "url": "css/app.64760d32.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "081d289905170a3f93aba46c37049c59",
+    "url": "index.html"
+  },
+  {
+    "revision": "996af9e2d542467bc6fd",
+    "url": "js/about.934c124e.js"
+  },
+  {
+    "revision": "cc7fc7eb990cb17c6f28",
+    "url": "js/app.fc94f8e8.js"
+  },
+  {
+    "revision": "6f93fb2e5f18372932d9",
+    "url": "js/chunk-vendors.b42a6c66.js"
+  },
+  {
+    "revision": "6c3f702b6a33169acd04beba4e260125",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
